ab960e658bca451c47e78d3af517d736013deccf
orynik <dkrovel@gmail.com>
2022-09-08 16:52:36 +0800

    Added font awesome and markuped header

--- src/components/layout/appHeader.vue
+++ src/components/layout/appHeader.vue
@@ -2,29 +2,34 @@
   import booksmedLogo from "@/components/icons/booksmedLogo.vue";
 </script>
 
+
 <template>
   <header class="page-header">
     <div class="page-header-top-nav">
       <booksmed-logo class="page-header-top-nav__logo"/>
 
-      <div class="page-header-top-nav__catalog">
-        КАТАЛОГ
+      <div class="page-header-top-nav-catalog">
+        <div class="page-header-top-nav-catalog-menu">
+          КАТАЛОГ
+        </div>
+
+        <input class="page-header-top-nav-search search-block" placeholder="Поиск медицинского оборудования">
       </div>
 
-      <input class="page-header-top-nav__search" placeholder="Поиск медицинского оборудования">
+      <div class="page-header-top-nav-contacts">
+        <span class="page-header-top-nav-contacts__callback contact-callback">
+          Заказать обратный звонок
+        </span>
 
-      <span class="page-header-top-nav__contact-callback contact-callback" >
-        Заказать обратный звонок
-      </span>
+        <span class="page-header-top-nav-contacts__phone contact-phone">
+          8 (3412) 65-08-77
+        </span>
 
-      <span class="page-header-top-nav__contact-phone contact-phone">
-        8 (3412) 65-08-77
-      </span>
-
-      <div class="page-header-top-nav__contact-panel contact-panel">
-        <span class="contact-panel__viber">V</span>
-        <span class="contact-panel__telegram">T</span>
-        <span class="contact-panel__whatsapp">W</span>
+        <div class="page-header-top-nav-contacts__panel-messagers contact-panel">
+          <span class="contact-panel__viber">V</span>
+          <span class="contact-panel__telegram">T</span>
+          <span class="contact-panel__whatsapp">W</span>
+        </div>
       </div>
     </div>
     <div class="page-header-nav">
@@ -39,7 +44,10 @@
       </ul>
 
       <ul class="page-header-nav-actions-list">
-        <li class="page-header-nav-actions-list__item">Сравнение</li>
+        <li class="page-header-nav-actions-list__item">
+          <font-awesome-icon icon="fa-solid fa-chart-column" />
+          Сравнение
+        </li>
         <li class="page-header-nav-actions-list__item">Корзина</li>
         <li class="page-header-nav-actions-list__item">Личный кабинет</li>
       </ul>
@@ -52,50 +60,72 @@
     margin-bottom: 53px;
     padding: 0 104px;
     background: var(--header-background-white);
+    box-shadow: 0px 10px 30px rgba(50, 87, 164, 0.1);
 
     &-top-nav {
       display: grid;
-      grid-template-columns: repeat(7,1fr);
+      grid-template-columns: 207px auto 482px;
       grid-template-rows: 40px;
-      gap: 100px 20px;
       align-items: center;
       justify-items: center;
-      margin-bottom: 24px;
+      padding-top: 18px;
 
       &__logo{
         justify-self: start;
       }
 
-      &__search {
-        grid-column: 3/5;
-        width: 300px;
-        height: 17px;
-        padding: 8px 15px 8px 40px;
+      &-contacts {
+        display: flex;
+        justify-self: end;
 
-        border-color: transparent;
-        border-radius: 10px;
-
-        box-shadow: inset 0px 0px 3px rgba(50, 87, 164, 0.25);
+        &__callback,
+        &__phone{
+          padding-right: 30px;
+        }
       }
 
-      &__contact-phone,
-      &__contact-panel{
-        justify-self: end;
+      &-catalog {
+        display: flex;
+        align-items: center;
+        justify-self: center;
+        justify-content: space-between;
+
+        &-menu {
+          padding-right: 50px;
+        }
       }
     }
 
-    &-nav{
+    &-nav {
       display: flex;
       justify-content: space-between;
       align-items: center;
 
       height: 34px;
+      padding: 16px 0;
     }
   }
 
   .page-header-nav-list,
   .page-header-nav-actions-list{
     display: flex;
-    gap: 28px;
+    gap: 32px;
+  }
+
+  .contact-callback{
+    color: var(--page-color-link);
+    text-decoration: underline;
+  }
+
+  .search-block {
+    min-width: 250px;
+    max-width: 450px;
+    height: 17px;
+    padding: 8px 15px 8px 40px;
+
+    border-color: transparent;
+    border-radius: 10px;
+
+    box-shadow: inset 0px 0px 3px rgba(50, 87, 164, 0.25);
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ import router from './router.js'
 * */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faChartColumn } from '@fortawesome/free-solid-svg-icons'
 library.add(faUserSecret)
+library.add(faChartColumn)
 
 createApp(App)
     .use(router)
