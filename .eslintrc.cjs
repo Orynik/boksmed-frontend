7c69d4408ab4f216edb4fec2e52c4ef26e7526e0
orynik <dkrovel@gmail.com>
2022-09-09 09:24:40 +0800

    Add prettier and eslint into project

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+
+  env: {
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    "vue/no-unused-vars": "error",
+  },
+};
