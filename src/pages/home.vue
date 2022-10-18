2e90408408568a95c4ac77d0dc6f4529d195c9e9
orynik <dkrovel@gmail.com>
2022-10-18 19:53:22 +0800

    Fix styles

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,38 +4,39 @@ import primedic from "@/assets/img/primedic.vue";
 
 <template>
   <main>
-    <section class="banner">
+    <section class="banner block">
       <div class="banner__wrapper">
-        <primedic />
-        <h3 class="banner--header">
-          Ручные и автоматические дефибрилляторы «Primedic»
-        </h3>
+        <div class="banner__body">
+          <primedic />
+          <h3 class="banner--header">
+            Ручные и автоматические дефибрилляторы «Primedic»
+          </h3>
 
-        <span class="banner--text"
-          >что то нужно написать, может быть качество и выгодные цены.
-        </span>
+          <span class="banner--text">
+            что то нужно написать, может быть качество и выгодные цены.
+          </span>
 
-        <div class="banner__action">
-          <a class="button button--blue">Перейти в каталог</a>
-          <i class="switch switch--prev"></i>
-          <i class="switch switch--next"></i>
+          <div class="banner__action">
+            <a class="button button--blue">Перейти в каталог</a>
+            <i class="switch switch--prev"></i>
+            <i class="switch switch--next"></i>
+          </div>
         </div>
+        <img src="@/assets/img/Banner_1.png" />
       </div>
-      <img src="@/assets/img/Banner_1.png" />
     </section>
   </main>
 </template>
 
 <style lang="scss">
 .banner {
-  display: flex;
-  flex-direction: row;
-  border-radius: 15px;
-  background-color: #fff;
-
-  box-shadow: -1px 0px 5px rgba(50, 87, 164, 0.3);
-
   &__wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  &__body {
     display: grid;
     padding: 146px 75px;
     max-width: 478px;
